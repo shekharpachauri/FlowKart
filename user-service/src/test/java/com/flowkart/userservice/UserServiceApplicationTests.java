84b83c3be1a5060f90672320dc3cccf3e75a623a
ShekharPachauri <shekhar.pachauri.dev@gmail.com>
2026-05-19 02:05:44 +0530

    Initial project setup - FlowKart microservices

---
+++ user-service/src/test/java/com/flowkart/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowkart.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
